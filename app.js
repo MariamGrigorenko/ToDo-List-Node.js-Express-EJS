1d90fe5fed5efd371ce9005369c275d914a452e1
Mariam Grigorenko <109353093+MariamGrigorenko@users.noreply.github.com>
2023-01-16 12:59:09 +0100

    Added MongoDB Atlas

--- app.js
+++ app.js
@@ -1,65 +1,144 @@
-// * Express *
+// Express
 const express = require("express");
 const app = express();
 
-// * Body Parser *
+// Body Parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// * Export JS files *
-const date = require(__dirname + "/date.js");
-
-//  * Consts *
-const port = process.env.PORT || 4000;
-
-// * Variables *
-let tasks = [];
-let workTasks = [];
-
-// * EJS *
-// Should be placed after "const app = express()"
+// EJS (Should be placed after "const app = express()")
 app.set("view engine", "ejs");
 
-// * Static Folder *
+// Lodash
+const lodash = require("lodash");
+
+// Static Folder
 app.use(express.static(__dirname + "/public"));
 
-// * Setting the current date and the main route
+// // Export JS files
+// const date = require(__dirname + "/date.js");
+
+//  Port
+const port = process.env.PORT || 4000;
+
+// Mongoose
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://MariamG:test123@cluster0.eafi9jo.mongodb.net/todolistDB");
+
+const tasksSchema = {
+  name: String,
+};
+
+const Task = mongoose.model("Task", tasksSchema);
+
+const task0 = new Task({
+  name: "Welcome to your todolist!",
+});
+
+const task1 = new Task({
+  name: "Hit the + button to add a new item.",
+});
+
+const task2 = new Task({
+  name: "<-- Hit this to delete an item.",
+});
+
+const listSchema = {
+  name: String,
+  tasks: [tasksSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
+// Variables
+
+// Setting the current date and the main route
 app.get("/", (req, res) => {
-  let currentDate = date.getDate();
-  res.render("list", { listTitle: currentDate, newTasks: tasks, route: "/" });
-});
-
-// * Getting and pushing the added tasks from the body of the main route
-app.post("/", (req, res) => {
-  let addedTask = req.body.newTask;
-  tasks.push(addedTask);
-  res.redirect("/");
-});
-
-//* Setting the work route
-app.get("/work", (req, res) => {
-  res.render("list", {
-    listTitle: "Work List",
-    newTasks: workTasks,
-    route: "/work",
+  // let currentDate = date.getDate();
+  Task.find({}, (err, foundTasks) => {
+    if (foundTasks.length === 0) {
+      Task.insertMany([task0, task1, task2], (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success!");
+        }
+        res.redirect("/");
+      });
+    } else {
+      res.render("list", { listTitle: "Today", newTasks: foundTasks });
+    }
   });
 });
 
-// * Getting and pushing the added tasks from the body of the work route
-app.post("/work", (req, res) => {
-  addedTask = req.body.newTask;
-  workTasks.push(addedTask);
-  res.redirect("/work");
+// Getting and pushing the added tasks from the body of the main route
+app.post("/", (req, res) => {
+  const addedTask = req.body.newTask;
+  const submitedTask = req.body.button;
+  const newTask = new Task({
+    name: addedTask,
+  });
+  if (submitedTask === "Today") {
+    newTask.save((err, reslut) => {
+      res.redirect("/");
+    });
+  } else {
+    List.findOne({ name: submitedTask }, (err, foundList) => {
+      foundList.tasks.push(newTask);
+      foundList.save((err, result) => {
+        res.redirect("/" + submitedTask);
+      });
+    });
+  }
 });
 
-// * Setting the about route
-app.get("/about", (req, res) => {
-  res.render("about");
+app.post("/delete", (req, res) => {
+  const checkboxId = req.body.checkbox;
+  const submitedTask = req.body.listName;
+  if (submitedTask === "Today") {
+    Task.findByIdAndRemove(checkboxId, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully removed task!");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOne({ name: submitedTask }, (err, foundList) => {
+      foundList.tasks.pull({ _id: checkboxId });
+      foundList.save(() => {
+        res.redirect("/" + submitedTask);
+      });
+    });
+  }
 });
 
-// * Porting our server *
+app.get("/:customListName", (req, res) => {
+  const customListName = lodash.capitalize(req.params.customListName);
+  if (customListName == "Favicon.ico") return;
+  console.log(customListName);
+  List.findOne({ name: customListName }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        const newList = new List({
+          name: customListName,
+          tasks: [task0, task1, task2],
+        });
+        newList.save((err, result) => {
+          res.redirect("/" + customListName);
+        });
+      } else {
+        res.render("list", { listTitle: foundList.name, newTasks: foundList.tasks });
+      }
+    }
+  });
+});
+
+// Porting our server *
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
 
-// ! very helpful: npx kill-port 3000 8000
+// * Very helpful: npx kill-port 3000 8000
